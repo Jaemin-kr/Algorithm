d0551acc29548f8666b357258328a5a91f878909
Jaemin-Mac <jaemin.ko98@gmail.com>
2023-01-21 22:05:34 +0900

    Simulation

---
+++ C++ Study/Barking_dog/0x0D/15683.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int n,m;
+int board1[10][10];
+int board2[10][10]; 
