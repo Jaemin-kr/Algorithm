3d8774f773154f6b01ab451ff0a0f9f162989443
Jaemin-Mac <jaemin.ko98@gmail.com>
2023-03-09 23:44:28 +0900

    230309 study

--- BaekJoon/1021.py
+++ BaekJoon/1021.py
@@ -45,3 +45,32 @@ for i in idx:
 print(cnt)
 
 #https://velog.io/@goplanit/Algorithm-%EB%B0%B1%EC%A4%80-1021%EB%B2%88-%ED%9A%8C%EC%A0%84%ED%95%98%EB%8A%94-%ED%81%90%ED%8C%8C%EC%9D%B4%EC%8D%AC
+
+
+import sys
+from collections import deque
+
+
+N, M = map(int, sys.stdin.readline().split())
+#que size, pop number
+
+idx = list(map(int, sys.stdin.readline().split()))
+deq = deque([i for i in range(1, N+1)])
+cnt = 0
+
+for i in idx:
+    while True:
+        if deq[0] == i:
+            deq.popleft()
+            break
+        else:
+            if deq.index(i) < len(deq)/2:
+                while deq[0] != i:
+                    deq.rotate(-1)
+                    cnt += 1
+            else:
+                while deq[0] != i:
+                    deq.rotate(1)
+                    cnt += 1
+
+print(cnt)
